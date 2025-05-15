ab4433b7f87380956066f5b461f537d16bcdff79
DhirajPatil98 <patildhiraj523@gmail.com>
2025-05-16 00:11:38 +0530

    Create clone.groovy

---
+++ vars/clone.groovy
@@ -0,0 +1,5 @@
+def call(){
+                echo "this is cloning the code"
+                git url :"https://github.com/DhirajPatil98/django-notes-app.git" , branch:"main"
+                echo "code cloning success."
+}

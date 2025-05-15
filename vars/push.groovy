32fa4e2ef4549179ff844a9b2dc75a53e0a3761c
DhirajPatil98 <patildhiraj523@gmail.com>
2025-05-16 01:16:11 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -4,6 +4,6 @@ echo " Pushing on docker hub"
 withCredentials([usernamePassword('credentialsId':"DOckerHubCreds", passwordVariable:"DockerHubPas", usernameVariable:"DockerHubUser")]){
 sh "docker login -u ${env.DockerHubUser} -p ${DockerHubPas}"
 }
-sh "docker push ${DockerHubUser}/${ProjectName}:${ImageTag}
+sh "docker push ${DockerHubUser}/${ProjectName}:${ImageTag}"
 
 }
